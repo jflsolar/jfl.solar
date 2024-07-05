328f48de49cc6b3f08ddc0e591e8a0fa5444e5dc
jfl.solar <jflsolarjay@gmail.com>
2024-07-05 11:46:50 +0800

    update bg and text

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "JFL Solar",
   description: "PV System Solutions for Residential, Industrial & Commercial",
+  openGraph: {
+    title: 'PV System Solutions for Residential, Industrial & Commercial',
+    description: 'Inquire now!',
+    images: ['/bg1.jpg']
+  }
 };
 
 export default function RootLayout({
@@ -16,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body >{children}</body>
     </html>
   );
 }
