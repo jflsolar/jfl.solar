b22118c7c718e38b7c3ded4e4c64471ed50868d2
jfl.solar <jflsolarjay@gmail.com>
2024-10-25 11:09:56 +0800

    chore(): update env label

--- components/Map.tsx
+++ components/Map.tsx
@@ -25,9 +25,7 @@ const Map: React.FC = () => {
   // }, []);
 
   return (
-    <LoadScript
-      googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}
-    >
+    <LoadScript googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY || ""}>
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
