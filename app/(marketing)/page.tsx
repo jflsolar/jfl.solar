33a6e8a90c56ed3964b1a14975fc57029039b8ae
jfl.solar <jflsolarjay@gmail.com>
2024-10-30 10:32:19 +0800

    feat(): update seo

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,10 +4,24 @@ import HomeCTA from "@/components/Home/HomeCTA";
 import Packages from "@/components/Home/Packages";
 // import Services from "@/components/Home/Services";
 import WhyChooseUs from "@/components/Home/WhyChooseUs";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>
+          JFL Solar - Reliable Solar PV Solutions in the Philippines
+        </title>
+        <meta
+          name="description"
+          content="Tailored Solar PV packages for efficiency, reliability, and savings. JFL Solar serves residential, commercial, and industrial projects."
+        />
+        <meta
+          name="keywords"
+          content="Solar PV, Solar Energy, Solar Panels, Inverters, Batteries, Renewable Energy, JFL Solar, Solar Philippines, Solar DIY Installers"
+        />
+      </Head>
       <Hero />
       <Brand />
       <Packages />
